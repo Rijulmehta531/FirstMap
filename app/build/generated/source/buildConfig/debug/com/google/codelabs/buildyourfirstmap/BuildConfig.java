8ff3b0fdd8c44b1ce6b4b31090b2295ab139df09
Rijul_Mrhta <mehtarijul9@gmail.com>
2023-09-28 21:47:48 -0400

    Initial commit

---
+++ app/build/generated/source/buildConfig/debug/com/google/codelabs/buildyourfirstmap/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.codelabs.buildyourfirstmap;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.codelabs.buildyourfirstmap";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  public static final String GOOGLE_MAPS_API_KEY = "AIzaSyA38MdXj32PCc2GxXU4XBGB-hMF6JX2qfI";
+}
